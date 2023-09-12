902a9c8cd0c313401677e5d099fb18cf0875ef8e
Aarondh02 <144380311+Aarondh02@users.noreply.github.com>
2023-09-12 16:56:38 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
+<script>
+  // Obtén el elemento del desplegable de "Planes"
+  var planesDropdown = document.getElementById("planesDropdown");
 
+  // Agrega un controlador de eventos al hacer clic en "Planes"
+  planesDropdown.addEventListener("click", function () {
+    var dropdownMenu = document.querySelector(".dropdown-menu");
+    // Alternar la visibilidad del desplegable
+    if (dropdownMenu.style.display === "block") {
+      dropdownMenu.style.display = "none"; // Oculta el desplegable si ya está visible
+    } else {
+      dropdownMenu.style.display = "block"; // Muestra el desplegable si está oculto
+    }
+  });
+</script>
+
+$(document).ready(function () {
+    $('.slick-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true
+    });
+
+    var planContainer = document.querySelector(".plan-container");
+    var planButtonsVisible = false;
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100 && !planButtonsVisible) {
+            planContainer.style.display = "block";
+            planButtonsVisible = true;
+        } else if (window.scrollY <= 100 && planButtonsVisible) {
+            planContainer.style.display = "none";
+            planButtonsVisible = false;
+        }
+    });
+});
